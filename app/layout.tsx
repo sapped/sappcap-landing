87bbd705cf8fecf5f0b9d1ed6f473cdf00c7300d
Edward Sapp <edward@demographiq.ai>
2024-12-03 14:11:57 -0500

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { AnalyticsProvider } from "@/components/analytics-provider"
+import { Metadata } from 'next'
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: 'Sapp Capital Advisors',
+  description: 'Bespoke Commercial Real Estate Services',
+  icons: {
+    icon: '/images/favicon.ico',
+  },
+}
 
 export default function RootLayout({
   children,
